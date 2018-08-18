fd91450102b1a957b8a185f113c1d02de4b9f005
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 03:35:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 
 
-Array.prototype.push.apply(vegetables, moreVegs);
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
 function johnLennonFacts(bestbeatle){
   var n = 0;
