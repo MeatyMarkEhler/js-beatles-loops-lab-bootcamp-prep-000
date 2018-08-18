9a4d214339550ca9c6a9ef7bab5f226abf22803c
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 03:18:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
+
+}
+/*
+
+
 function theBeatlesPlay (musicians, instruments) {
   var array = []
   for ( i = 0; i>musicians.length; i++) {
     array.apply(`${musicians[i]} plays ${instruments[i]}`);
   }
   return array
-}
-/*
-Array.prototype.push.apply(vegetables, moreVegs);
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 */
