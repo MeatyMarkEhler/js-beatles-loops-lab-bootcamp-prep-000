50b3633985292b4cdb0f9555f7a13e267b3e80fc
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 03:39:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(bestbeatle){
 
 function theBeatlesPlay (musicians, instruments) {
   var array = []
-  for ( i = 0; i>musicians.length; i++) {
+  for ( i = 0; i<musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return array
